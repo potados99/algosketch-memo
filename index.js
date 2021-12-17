d98044c0859766ffaa31bfb2ed66d753f255d5dd
potados99 <potados99@gmail.com>
2021-12-18 01:26:20 +0900

    feat: 메모 임시저장

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('aaaa');
+let memo = '아직메모없음!';
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    content: memo
+  });
+});
+
+app.post('/', (req, res) => {
+  const {content} = req.body;
+
+  memo = content;
+
+  res.send();
+});
+
+app.listen(process.env.PORT);
